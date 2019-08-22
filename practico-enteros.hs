e2acea642be77b5c0bde8acdc1fc73100662278d
Joaquin Ruiz <joacoruiz7m@gmail.com>
2019-08-22 14:24:29 -0300

    Se empieza practico enteros

---
+++ practico-enteros.hs
@@ -0,0 +1,44 @@
+module PracticoEnteros where 
+
+sumatoria::Int->Int
+sumatoria n 
+        | n == 0 = 0
+        | n > 0 = n + sumatoria (n-1);
+
+factorial::Int->Int
+factorial n 
+        | n == 0 = 1
+        | n > 0 = n* factorial(n-1);
+dosALaN::Int->Int
+dosALaN n 
+        | n == 0 = 1
+        | n > 0 = 2 * dosALaN(n-1);
+exponencial::Int->Int->Int
+exponencial n n2 
+            | n==0 && n2 == 0 = error "Math error"
+            | n == 0 = 0
+            | n2 == 0 = 1
+            | otherwise = n * exponencial n (n2-1);
+copias::String->Int->String
+copias s n
+        | n<0 = error "Enter a positive number"
+        | n == 1 = s
+        | otherwise =  s ++ copias s (n-1);
+productoNatConSuma::Int->Int->Int
+productoNatConSuma n1 n2
+                        | n2 == 0 = 0
+                        | n2 > 0 = n1 + productoNatConSuma n1 (n2-1);
+cocienteConResta::Int->Int->Int
+cocienteConResta n1 n2 
+                        | n2 == 0 = error "Can´t divide by 0"
+                        | n1 == 0 = 0 
+                        | (n1 > 0) && (n2 > n1) = 0
+                        | n2 > 0 = 1 + cocienteConResta (n1-n2) n2;
+modulo::Int->Int->Int
+modulo n1 n2
+            | n2 == 0 = error "Undefined"
+            | n1 == 0 = 0
+            | (n1 > 0) && (n2>n1) = n1
+            | n2 > 0 = modulo (n1-n2) n2;
+            
+            
