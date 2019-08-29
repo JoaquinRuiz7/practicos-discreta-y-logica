66107f4c1361bb0c1464d0d362030497d301d687
Joaquin Ruiz <joacoruiz7m@gmail.com>
2019-08-29 00:22:19 -0300

    Funcion primer divisor terminada

--- practico-enteros.hs
+++ practico-enteros.hs
@@ -40,5 +40,17 @@ modulo n1 n2
             | n1 == 0 = 0
             | (n1 > 0) && (n2>n1) = n1
             | n2 > 0 = modulo (n1-n2) n2;
-            
+
+esDivisor::Int->Int->Bool
+esDivisor n n2 
+        | modulo n n2 == 0 = True
+        | otherwise = False;
+
+primerDivisor::Int->Int
+primerDivisor n = primerDivisorAux n 2;
+{-Pre: n1 empieza en 2-}
+primerDivisorAux::Int->Int->Int
+primerDivisorAux n n1 
+        | esDivisor n n1 = n1
+        | otherwise = primerDivisorAux n (n1+1);
             
