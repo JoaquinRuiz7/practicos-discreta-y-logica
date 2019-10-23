3ff3ed461f80a970938408145f5b80d575979722
Joaquin <joacoruiz7m@gmail.com>
2019-10-23 01:49:47 -0300

    Empece lo de 0 a 6

--- Conteo.hs
+++ Conteo.hs
@@ -28,3 +28,31 @@ posiblesNBits::Int->[[Int]]
 posiblesNBits 0 = []
 posiblesNBits 1 = agregar01 []
 posiblesNBits n = agregarBit (posiblesNBits (n-1));
+-- Recibe el numero de bits,retorna la cantidad de numeros que se pueden hacer con esa cantidad de bits.
+cantidadCasos::Int->Int
+cantidadCasos 0 = 0
+cantidadCasos n = length (posiblesNBits n);
+{--
+Empezaremos entonces por implementar numEntre, que recibirá 2 enteros m y n y generará la
+lista con todos los números entre m y n, incluyendo a éstos.
+Ejemplo numEntre 2 5 = [2,3,4,5]
+--}
+numEntre::Int->Int->[Int]
+numEntre 0 0 = []
+numEntre a b 
+    | a <= b = a:numEntre (a+1) b
+    | a > b = [];
+--Ejemplo agregarTodos [1,2,3] [4,7] = [[1,4,7],[2,4,7],[3,4,7]]
+agregarTodos::[Int]->[Int]->[[Int]]
+agregarTodos [] l2 = []
+agregarTodos (x:xs)  l2 = (x:l2):agregarTodos xs l2;
+numerosAlistasIndividuales::Int->[[Int]]
+numerosAlistasIndividuales n = numerosAlistasIndividualesAux 0 n;
+numerosAlistasIndividualesAux::Int->Int->[[Int]]
+numerosAlistasIndividualesAux  a b
+    | a > b = []
+    | a <= b = [a]:numerosAlistasIndividualesAux (a+1) b;
+--agregar06 [1] = [[0,1],[1,1],[2,1],[3,1],[4,1],[5,1],[6,1]]
+agregar06::[Int]->[[Int]]
+agregar06 [] = numerosAlistasIndividuales 6;
+agregar06 (x:xs) = undefined
