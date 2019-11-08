0430efdf0fe858e7c9ef9f906c0c11921721f400
Joaquin <joacoruiz7m@gmail.com>
2019-11-08 17:51:50 -0300

    Practico terminado , duda lazos

--- Grafos.hs
+++ Grafos.hs
@@ -17,7 +17,7 @@ Programar la funcion agregarArista::Grafo->Vertice->Vertice->Grafo
 que dado un grafo y dos vertices v1 y v2, agrego la arista que va desde el vertice a al vertice b en el grafo
 --}
 grafoPrueba::Grafo
-grafoPrueba = [(1,[2,3]),(2,[1,3]),(3,[1,2]) ,(4,[3]) ];
+grafoPrueba = [(1,[1,1,2,3]),(2,[1,3]),(3,[1,2]) ,(4,[3]) ];
 
 agregarArista::Grafo->Vertice->Vertice->Grafo
 agregarArista [] v1 v2= []
@@ -54,5 +54,27 @@ contiene e (x:xs)
 gradoEntrada::Grafo->Vertice->Int
 gradoEntrada [] v = 0
 gradoEntrada ((vertice,l):xs) v
-    | contiene v l = 1+gradoEntrada xs v
+    | contiene v l && vertice /= v = 1+gradoEntrada xs v
     | otherwise = gradoEntrada xs v;
+{--
+implementar una funcion gradoSalida::Grafo->Vertice->Int
+--}
+gradoSalida::Grafo->Vertice->Int
+gradoSalida [] v = 0
+gradoSalida ( (vertice,l):xs) v
+    | vertice == v = length l - (cantidadLazos ((vertice,l):[]) v) 
+    | otherwise = gradoSalida xs v; 
+{--
+Implementar una funcion que dado un grafo y un vertice retorne e grado de incidencia 
+del vertice
+--}
+cantidadLazos::Grafo->Vertice->Int
+cantidadLazos [] v = 0
+cantidadLazos ( (vertice, [] ):xs ) v = 0
+cantidadLazos ( (vertice, (y:ys) ):xs ) v
+    | vertice == y = 1+cantidadLazos ( (vertice,ys):xs ) v
+    | otherwise = cantidadLazos ( (vertice,ys):xs ) v;
+
+gradoIncidencia::Grafo->Vertice->Int
+gradoIncidencia [] v = 0
+gradoIncidencia g v = ( ( cantidadLazos g v ) * 2 ) +gradoEntrada g v + gradoSalida g v;
